64360fa46cb0f746ce9d4a6b033484b6fbc1c5e1
Felix <felixtosar@gmail.com>
2023-10-16 22:07:26 +0200

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,9 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+
+		// Modifica el código fuente para que muestra por pantalla "Hello world!\n"
+		System.out.println("Algo no esta correcto, el test está fallando!");
 	}
 
 }
